84cbfbb18f52f1608a25ccd30ea4f5762093049a
eunssol <eunssol01@gmail.com>
2021-07-17 18:52:07 +0900

    eunsol

--- bsp/src/main/java/book/BookDao.java
+++ bsp/src/main/java/book/BookDao.java
@@ -1,5 +1,7 @@
 package book;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,7 +12,27 @@ public class BookDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
+	public List<BookVo> selectAll(BookVo vo) {
+		return sqlSession.selectList("book.selectAll", vo);
+	}
+	
+	public int count(BookVo vo) {
+		return sqlSession.selectOne("book.count", vo);
+	}
+	
+	public BookVo detail(BookVo vo) {
+		return sqlSession.selectOne("book.detail", vo);
+	}
+	
 	public int insert(BookVo vo) {
-		return sqlSession.insert("book.insert",vo);
+		return sqlSession.insert("book.insert", vo);
+	}
+	
+	public int update(BookVo vo) {
+		return sqlSession.update("book.update", vo);
+	}
+	
+	public int delete(BookVo vo) {
+		return sqlSession.delete("book.delete", vo);
 	}
 }
--- bsp/src/main/java/book/BookVo.java
+++ bsp/src/main/java/book/BookVo.java
@@ -2,107 +2,145 @@ package book;
 
 import java.util.Date;
 
-public class BookVo {
+import util.CommonVo;
 
-	private int B_No;
-	private int B_ImgNo;
-	private int B_CtgNo1;
-	private int B_CtgNo2;
-	private String B_Title;
-	private String B_Content;
-	private String B_Author;
-	private String B_Publisher;
-	private String B_Isbn;
-	private String B_Stock;
-	private Date B_Regdate;
-	private Date B_Intodate;
-	private int B_Price;
-	private int B_Views;
+public class BookVo extends CommonVo {
+
+	private int b_no;
+	private int b_imgNo;
+	private int b_ctgNo1;
+	private int b_ctgNo2;
+	private String b_title;
+	private String b_content;
+	private String b_author;
+	private String b_publisher;
+	private String b_isbn;
+	private String b_stock;
+	private Date b_regdate;
+	private Date b_intodate;
+	private int b_price;
+	private int b_views;
+	private String b_index;
+	private int b_pages;
+	private String b_introauthor;
+	private String b_intorbook;
+	private int r_id;
 	
-	public int getB_No() {
-		return B_No;
+	public int getB_no() {
+		return b_no;
 	}
-	public void setB_No(int b_No) {
-		B_No = b_No;
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
 	}
-	public int getB_ImgNo() {
-		return B_ImgNo;
+	public int getB_imgNo() {
+		return b_imgNo;
 	}
-	public void setB_ImgNo(int b_ImgNo) {
-		B_ImgNo = b_ImgNo;
+	public void setB_imgNo(int b_imgNo) {
+		this.b_imgNo = b_imgNo;
 	}
-	public int getB_CtgNo1() {
-		return B_CtgNo1;
+	public int getB_ctgNo1() {
+		return b_ctgNo1;
 	}
-	public void setB_CtgNo1(int b_CtgNo1) {
-		B_CtgNo1 = b_CtgNo1;
+	public void setB_ctgNo1(int b_ctgNo1) {
+		this.b_ctgNo1 = b_ctgNo1;
 	}
-	public int getB_CtgNo2() {
-		return B_CtgNo2;
+	public int getB_ctgNo2() {
+		return b_ctgNo2;
 	}
-	public void setB_CtgNo2(int b_CtgNo2) {
-		B_CtgNo2 = b_CtgNo2;
+	public void setB_ctgNo2(int b_ctgNo2) {
+		this.b_ctgNo2 = b_ctgNo2;
 	}
-	public String getB_Title() {
-		return B_Title;
+	public String getB_title() {
+		return b_title;
 	}
-	public void setB_Title(String b_Title) {
-		B_Title = b_Title;
+	public void setB_title(String b_title) {
+		this.b_title = b_title;
 	}
-	public String getB_Content() {
-		return B_Content;
+	public String getB_content() {
+		return b_content;
 	}
-	public void setB_Content(String b_Content) {
-		B_Content = b_Content;
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
 	}
-	public String getB_Author() {
-		return B_Author;
+	public String getB_author() {
+		return b_author;
 	}
-	public void setB_Author(String b_Author) {
-		B_Author = b_Author;
+	public void setB_author(String b_author) {
+		this.b_author = b_author;
 	}
-	public String getB_Publisher() {
-		return B_Publisher;
+	public String getB_publisher() {
+		return b_publisher;
 	}
-	public void setB_Publisher(String b_Publisher) {
-		B_Publisher = b_Publisher;
+	public void setB_publisher(String b_publisher) {
+		this.b_publisher = b_publisher;
 	}
-	public String getB_Isbn() {
-		return B_Isbn;
+	public String getB_isbn() {
+		return b_isbn;
 	}
-	public void setB_Isbn(String b_Isbn) {
-		B_Isbn = b_Isbn;
+	public void setB_isbn(String b_isbn) {
+		this.b_isbn = b_isbn;
 	}
-	public String getB_Stock() {
-		return B_Stock;
+	public String getB_stock() {
+		return b_stock;
 	}
-	public void setB_Stock(String b_Stock) {
-		B_Stock = b_Stock;
+	public void setB_stock(String b_stock) {
+		this.b_stock = b_stock;
 	}
-	public Date getB_Regdate() {
-		return B_Regdate;
+	public Date getB_regdate() {
+		return b_regdate;
 	}
-	public void setB_Regdate(Date b_Regdate) {
-		B_Regdate = b_Regdate;
+	public void setB_regdate(Date b_regdate) {
+		this.b_regdate = b_regdate;
 	}
-	public Date getB_Intodate() {
-		return B_Intodate;
+	public Date getB_intodate() {
+		return b_intodate;
 	}
-	public void setB_Intodate(Date b_Intodate) {
-		B_Intodate = b_Intodate;
+	public void setB_intodate(Date b_intodate) {
+		this.b_intodate = b_intodate;
 	}
-	public int getB_Price() {
-		return B_Price;
+	public int getB_price() {
+		return b_price;
 	}
-	public void setB_Price(int b_Price) {
-		B_Price = b_Price;
+	public void setB_price(int b_price) {
+		this.b_price = b_price;
 	}
-	public int getB_Views() {
-		return B_Views;
+	public int getB_views() {
+		return b_views;
 	}
-	public void setB_Views(int b_Views) {
-		B_Views = b_Views;
+	public void setB_views(int b_views) {
+		this.b_views = b_views;
 	}
+	public String getB_index() {
+		return b_index;
+	}
+	public void setB_index(String b_index) {
+		this.b_index = b_index;
+	}
+	public int getB_pages() {
+		return b_pages;
+	}
+	public void setB_pages(int b_pages) {
+		this.b_pages = b_pages;
+	}
+	public String getB_introauthor() {
+		return b_introauthor;
+	}
+	public void setB_introauthor(String b_introauthor) {
+		this.b_introauthor = b_introauthor;
+	}
+	public String getB_intorbook() {
+		return b_intorbook;
+	}
+	public void setB_intorbook(String b_intorbook) {
+		this.b_intorbook = b_intorbook;
+	}
+	public int getR_id() {
+		return r_id;
+	}
+	public void setR_id(int r_id) {
+		this.r_id = r_id;
+	}
+	
 	
 	
 	

c7012989863d973539069012a5ab7cc63d13f8c4
eunssol <eunssol01@gmail.com>
2021-07-17 18:52:17 +0900

    eunsol

---
+++ bsp/src/main/java/book/BookService.java
@@ -0,0 +1,13 @@
+package book;
+
+import java.util.List;
+
+
+public interface BookService {
+	List<BookVo> selectAll(BookVo vo);
+	BookVo deatil(BookVo vo);
+	int insert(BookVo vo);
+	BookVo edit(BookVo vo);
+	int update(BookVo vo);
+	int delete(BookVo vo);
+}

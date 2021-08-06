a121a461cc08deb10cd35ee667ed9735361e9fd0
chanho <cksgh901@naver.com>
2021-08-06 19:57:06 +0900

    오류수정

--- bsp/src/main/java/order/OrderVo.java
+++ bsp/src/main/java/order/OrderVo.java
@@ -36,7 +36,15 @@ public class OrderVo extends CommonVo {
 	private String m_name;
 	private String m_email;
 	private String m_email_d;
-	
+
+	public  OrderVo() {
+		
+		this.pageRow = 10;
+		this.reqPage = 1;
+		this.pageRange = 10;
+		this.orderby ="pb_orderdate";
+		this.direct = "DESC";
+	}
 	
 	
 
